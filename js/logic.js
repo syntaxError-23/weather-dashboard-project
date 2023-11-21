db37e949f0a3ad9691f2cc63531f92cd5ce8f45f
Josef K <ahmed.abshir91@gmail.com>
2023-11-21 22:25:37 +0000

    Updates index.html, styles and logic - styling changes made and some bug fixes in logic

--- js/logic.js
+++ js/logic.js
@@ -12,9 +12,9 @@
 var inputEl = $('#search-input');
 var weatherSection = $('#today');
 var forecastSection = $('#forecast');
-var searchesSection = $('#searches');
+var historyDiv = $('#history');
 var searchHistoryButtonsDiv = $('#search-history-container');
-var searchesBtn = $('#search-history-button');
+var clearBtn = $('#clear-history');
 
 //converts m/s to mph
 var windSpeedConversion = 2.2369362921;
@@ -25,12 +25,19 @@ $('#search-button').on('click', event => {
 
     var cityName = inputEl.val();
 
+    if(cityName === ''){
+        alert('Please enter a city');
+        return;
+    }
+
     // API URLs
     var queryForecastURL = ('http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + openWeatherKey + '&units=metric');
     var queryWeatherURL  = ('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + openWeatherKey + '&units=metric');
 
     getWeatherData(queryForecastURL, queryWeatherURL);
     saveCity(cityName);
+
+    cityName.text('');
 })
 
 //Get forecast and weather data 
@@ -62,37 +69,59 @@ var getWeatherData = (forecastQuery, weatherQuery) => {
 var saveCity = city => {
     
     var searchHistoryArray = JSON.parse(localStorage.getItem('previous-searches')) || [];
-    console.log(searchHistoryArray);
+    console.log('SearchHistoryArray: ' +  searchHistoryArray);
     searchHistoryArray.push(city);
     localStorage.setItem('previous-searches', JSON.stringify(searchHistoryArray));
 }
 
- searchesBtn.on('click', () => {
-    
     searchHistoryButtonsDiv.empty();
-    var searches = JSON.parse(localStorage.getItem('previous-searches'));  
+    var searches = JSON.parse(localStorage.getItem('previous-searches')) || [];  
     console.log(searches);
 
     //loop through searches and create button for previous searches
-    for(var i=0; i<searches.length; i++){
-        var searchHistoryItem = $('<button>').text(searches[i]).addClass('search-history-item');
+    
+    if(searches.length >= 5){
         
-        searchHistoryButtonsDiv.append(searchHistoryItem);
+        for(var i=0; i<5; i++){
+        
+            if((searches[searches.length - (i+1)]) != ''){
+                var searchHistoryItem = $('<button>').text(searches[searches.length - (i+1)]).addClass('search-history-item btn btn-primary my-1')
+                searchHistoryButtonsDiv.append(searchHistoryItem).addClass('d-flex flex-column');
+            }
+    
+        }
     }
 
-    searchesSection.append(searchHistoryButtonsDiv);
-    //add class to new buttons
-    //create delegated event listener for click of new button
+    else{ 
+        for(var j=0; j<searches.length; j++){
+            var searchHistoryItem = $('<button>').text(searches[j]).addClass('search-history-item btn btn-primary my-1');
+            searchHistoryButtonsDiv.prepend(searchHistoryItem).addClass('d-flex flex-column');
+        }
 
-    $(document).on('click', '.search-history-item', event => {
-        desiredCity = event.target.textContent;
-
-        var searchForecastURL = ('http://api.openweathermap.org/data/2.5/forecast?q=' + desiredCity + '&appid=' + openWeatherKey + '&units=metric');
-        var searchWeatherURL  = ('https://api.openweathermap.org/data/2.5/weather?q=' + desiredCity + '&appid=' + openWeatherKey + '&units=metric');
         
-        getWeatherData(searchForecastURL, searchWeatherURL);
-    })
-    //on button click, clear display and show data corresponding to city name of new button
+    }
+   
+
+    clearBtn.on('click', () => {
+
+        var clearConfirm =  confirm('Are you sure you want clear your history?');
+     
+        if(clearConfirm === true){
+            searchHistoryButtonsDiv.empty();
+            localStorage.clear();
+        }
+     })
+
+    historyDiv.append(searchHistoryButtonsDiv).addClass('d-flex flex-column-reverse my-2');
+
+
+$(document).on('click', '.search-history-item', event => {
+    desiredCity = event.target.textContent;
+
+    var searchForecastURL = ('http://api.openweathermap.org/data/2.5/forecast?q=' + desiredCity + '&appid=' + openWeatherKey + '&units=metric');
+    var searchWeatherURL  = ('https://api.openweathermap.org/data/2.5/weather?q=' + desiredCity + '&appid=' + openWeatherKey + '&units=metric');
+    
+    getWeatherData(searchForecastURL, searchWeatherURL);
 })
 
 
@@ -100,10 +129,10 @@ var saveCity = city => {
 var displayWeather = weatherData => {
 
     var weatherDiv = $('<div>').addClass('weather-div');
-    var HeaderDiv = $('<div>');
+    var HeaderDiv = $('<div>').addClass('date-and-icon');
 
     var weatherHeader = $('<h2>');
-    weatherHeader.text(weatherData.name + ', (' + weatherData.sys.country + ') ' + dayjs().format('DD/MM/YY'));
+    weatherHeader.text(weatherData.name + ' (' + weatherData.sys.country + '), ' + dayjs().format('DD/MM/YY'));
     weatherIconURL = ('https://openweathermap.org/img/wn/' + weatherData.weather[0].icon + '@2x.png');
     var weatherIcon = $('<img>').attr('src', weatherIconURL);
     console.log(weatherIconURL);
@@ -123,7 +152,7 @@ var displayWeather = weatherData => {
 var displayForecast = forecastData => {
 
     var forecastDiv = $('<div>').addClass('forecast-div');
-
+    var forecastHeader = $('<h2>').text('5 Day Forecast').addClass('forecast-header');
     var ifCounter = 0;
 
     for(var i=0; i<40; i++){
@@ -146,16 +175,16 @@ var displayForecast = forecastData => {
             var todaysWind = currentItem.wind.speed;
             var todaysWindEl = $('<li>').text('Wind: ' + todaysWind + ' mph');
             var todaysHumidity = currentItem.main.humidity;
-            var todaysHumidityEl = $('<li>').text('Humdidty: ' + todaysHumidity + '%');
+            var todaysHumidityEl = $('<li>').text('Humidity: ' + todaysHumidity + '%');
    
             dailyInfoList.append(currentDateEl).append(todaysIcon).append(todaysTempEl).append(todaysWindEl).append(todaysHumidityEl)
-            oneDayForecast.append(dailyInfoList).css('background-color', 'red');
+            oneDayForecast.append(dailyInfoList).css('background-color', 'rgba(2,117,216,0.5)');
             forecastDiv.append(oneDayForecast);
 
             ifCounter++;
         }
     }
 
-    forecastSection.append(forecastDiv);
+    forecastSection.append(forecastHeader).append(forecastDiv);
 
 }
